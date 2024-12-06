b7f13380daf1aad6dda11dbcbd0349c2d7c1e32f
richard06 <richardmacabus06@gmail.com>
2024-12-06 16:57:45 +0800

    fixing the transition on each div project, adding a trasintion on the icons, add all the screenshot image in the page, provide the title and description,

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import style from "./style.css"
 
 console.log("hello");
+
+const project = document.querySelector('.projects')
+const everyproj = document.querySelectorAll('.project-screenshot')
+console.log(project);
+
+project.addEventListener('mouseover', (e) => {
+    // console.log(e.target);
+    if (e.target.classname === "project") {
+        alert("jello")
+        // everyproj.forEach((el, i) => {
+        // });
+    }
+});
